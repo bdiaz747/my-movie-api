0cdca55e551ce0843f8e18cf68953d03bc8f77cf
Brahian Diaz G <brahian.diazgarcia@gmail.com>
2023-05-04 23:09:51 -0500

    Modificando la documentación con Sawggwer

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+app.title = "My app con FastAPI" # para cambiar el titulo de la documentacion con Sawgger
+app.version = "0.0.1" # para cambiar el la verión de la documentacion con Sawgger
+
+@app.get('/', tags = ['home']) # para cambiar la ruta
+
 
 def message():
     return 'A dormir papilin, feliz noche'
