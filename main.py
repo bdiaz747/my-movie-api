32ad53001cd195b448cdfb8d745c53b6309c187a
Brahian Diaz G <brahian.diazgarcia@gmail.com>
2023-05-10 23:05:32 -0500

    Creación de esquemas

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional
 
  
 
@@ -8,6 +10,15 @@ app = FastAPI()
 app.title = "My app con FastAPI" # para cambiar el titulo de la documentacion con Sawgger
 app.version = "0.0.1" # para cambiar el la verión de la documentacion con Sawgger
 
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
+    
+    
 movies = [
     {
 		"id": 1,
@@ -59,26 +70,19 @@ def get_movies_by_title(title: str):
     return title
 
 @app.post('/movies', tags=['movies'])
-def create_movie(id: int = Body(), title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
-    movies.append({
-        "id": id,
-        "title": title,
-        "overview": overview,
-        "year": year,
-        "rating": rating,
-        "category": category
-    })
+def create_movie(movie: Movie):
+    movies.append(movie)
     return movies
 
 @app.put(f'/movies/{id}', tags=['movies'])
-def update_movie(id: int, title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
+def update_movie(id: int, movie: Movie ):
     for item in movies: 
         if item['id'] == id:
-            item['title'] = title,
-            item['overview:'] = overview,
-            item['year'] = year,
-            item['rating'] = rating
-            item['category'] = category
+            item['title'] = movie.title
+            item['overview:'] = movie.overview
+            item['year'] = movie.year
+            item['rating'] = movie.rating
+            item['category'] = movie.category
             return movies
         
 @app.delete(f'/movies/{id}', tags=['movies'])
