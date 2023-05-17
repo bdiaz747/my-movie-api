60843acc51ae1ae1ec47b16e94c7f78d065111af
Brahian Diaz G <brahian.diazgarcia@gmail.com>
2023-05-16 21:53:38 -0500

    Trabajando con esuqemas

--- main.py
+++ main.py
@@ -70,30 +70,23 @@ def get_movies_by_title(title: str):
     return title
 
 @app.post(f'/movies', tags=['movies'])
-def create_movie(id: int = Body(), title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
-    movies.append({
-        "id": id,
-        "title": title, 
-        "overview": overview,   
-        "year": year,
-        "rating": rating,
-        "category": category,
-    })
+def create_movie(movie: Movie):
+    movies.append(movie)
     return movies
 
 @app.put(f'/movies/{id}', tags=['movies'])
-def update_movie(id: int, title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
+def update_movie(id: int, movie: Movie):
     for item in movies: 
             if item['id'] == id:
-                item['title'] = title,
-                item['overview'] = overview,
-                item['year'] = year,
-                item['rating'] = rating,
-                item['category'] = category
+                item['title'] = movie.title
+                item['overview'] = movie.overview
+                item['year'] = movie.year
+                item['rating'] = movie.rating
+                item['category'] = movie.category
                 return movies
         
 @app.delete(f'/movies/{id}', tags=['movies'])
-def delate_movie(id: int, ):
+def delate_movie(id: int):
     for item in movies: 
         if item['id'] == id:
             movies.remove(item)
